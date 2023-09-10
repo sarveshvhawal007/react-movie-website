6365eb3af4c730abc9a51080d6ce83be9f93e6f6
Sarvesh Vhawal <isarvesh@amazon.com>
2023-09-10 13:59:30 +0530

    added basename to browserRouter

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Service from "./components/pages/Service";
 function App() {
   return (
     <div className="App">
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
             <Navbar/>
             <Routes>
                 <Route path='/' element={<Home/>}/>
